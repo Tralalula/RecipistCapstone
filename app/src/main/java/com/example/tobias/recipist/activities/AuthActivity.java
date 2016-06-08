8c4c217bff1d6052908da098f3bc24acf3cacaf9
Tobias <qenzoz@gmail.com>
2016-06-08 20:46:53 +0200

    Task 1 - Project Setup

---
+++ app/src/main/java/com/example/tobias/recipist/activities/AuthActivity.java
@@ -0,0 +1,7 @@
+package com.example.tobias.recipist.activities;
+
+/**
+ * Created by Tobias on 08-06-2016.
+ */
+public class AuthActivity {
+}
---
+++ app/src/main/java/util/Utilities.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by Tobias on 08-06-2016.
+ */
+public class Utilities {
+}
