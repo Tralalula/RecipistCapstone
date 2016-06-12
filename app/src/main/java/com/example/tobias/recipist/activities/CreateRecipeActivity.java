3522573a98262dc39d55b8cf3312ff2d03c9d9ea
Tobias <qenzoz@gmail.com>
2016-06-13 00:26:29 +0200

    Add create recipe icon button to toolbar

--- app/src/main/java/com/example/tobias/recipist/activities/CreateRecipeActivity.java
+++ app/src/main/java/com/example/tobias/recipist/activities/CreateRecipeActivity.java
@@ -1,7 +1,17 @@
 package com.example.tobias.recipist.activities;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.tobias.recipist.R;
+
 /**
  * Created by Tobias on 08-06-2016.
  */
-public class CreateRecipeActivity {
+public class CreateRecipeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe);
+    }
 }
--- app/src/main/java/com/example/tobias/recipist/activities/MainActivity.java
+++ app/src/main/java/com/example/tobias/recipist/activities/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.tobias.recipist.activities;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -8,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.tobias.recipist.R;
 
@@ -34,4 +40,27 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        // http://androidforums.com/threads/how-to-change-color-of-menu-item-icon-in-android.916282/
+//        Drawable createRecipeIcon = menu.findItem(R.id.action_create_recipe).getIcon();
+//        if (createRecipeIcon != null) {
+////            createRecipeIcon.setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
+//        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_recipe:
+                Intent intent = new Intent(this, CreateRecipeActivity.class);
+                startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
