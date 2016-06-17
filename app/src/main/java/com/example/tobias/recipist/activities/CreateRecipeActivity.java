c9c134ec2cdf83a1cb2f05293cdae19c82cf6e72
Tobias <qenzoz@gmail.com>
2016-06-17 21:32:30 +0200

    Update camera recipe thumbnail function

--- app/src/main/java/com/example/tobias/recipist/activities/CreateRecipeActivity.java
+++ app/src/main/java/com/example/tobias/recipist/activities/CreateRecipeActivity.java
@@ -5,6 +5,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,32 +18,50 @@ import android.widget.ImageView;
 
 import com.example.tobias.recipist.R;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import util.Utilities;
 
-/**
- * Created by Tobias on 08-06-2016.
- */
-// For handling camera images I used the following guide: http://www.theappguruz.com/blog/android-take-photo-camera-gallery-code-sample
+// For handling camera images I used the following guides:
+// http://www.theappguruz.com/blog/android-take-photo-camera-gallery-code-sample
+// http://blog-emildesign.rhcloud.com/?p=590
+// https://developer.android.com/training/camera/photobasics.html
 public class CreateRecipeActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int REQUEST_CAMERA = 1337;
     private static final int SELECT_FILE = 1338;
 
-    private String mUserChosenTask = "";
+    private String mUserChosenTask;
 
+    private static final String BITMAP_STORAGE_KEY = "viewbitmap";
     private ImageView mRecipeImage;
+    private Bitmap mRecipeBitmapImage;
+
+    private String mCurrentTimeMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
 
+        mCurrentTimeMillis = System.currentTimeMillis() + "";
+
+        mRecipeBitmapImage = null;
         mRecipeImage = (ImageView) findViewById(R.id.recipe_image);
         if (mRecipeImage != null) mRecipeImage.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(BITMAP_STORAGE_KEY, mRecipeBitmapImage);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mRecipeBitmapImage = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
+        mRecipeImage.setImageBitmap(mRecipeBitmapImage);
     }
 
     @Override
@@ -77,6 +97,8 @@ public class CreateRecipeActivity extends AppCompatActivity implements View.OnCl
 
     private void cameraIntent() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = new File(Environment.getExternalStorageDirectory() + File.separator + mCurrentTimeMillis + "image.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
         startActivityForResult(intent, REQUEST_CAMERA);
     }
 
@@ -103,15 +125,39 @@ public class CreateRecipeActivity extends AppCompatActivity implements View.OnCl
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == SELECT_FILE) onSelectFromGalleryResult(data);
-            else if (requestCode == REQUEST_CAMERA) onCaptureImageResult(data);
+            else if (requestCode == REQUEST_CAMERA) {
+                onCaptureImageResult();
+            }
         }
+    }
+
+    public Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        int inSampleSize = 1;
+        if (height > reqHeight) inSampleSize = Math.round((float) height / (float) reqHeight);
+        int expectedWidth = width / inSampleSize;
+        if (expectedWidth > reqWidth) inSampleSize = Math.round((float) width / (float) reqWidth);
+
+        options.inSampleSize = inSampleSize;
+        options.inJustDecodeBounds = false;
+        mRecipeBitmapImage = BitmapFactory.decodeFile(path, options);
+        return mRecipeBitmapImage;
     }
 
     private void onSelectFromGalleryResult(Intent data) {
         Bitmap bitmap = null;
         if (data != null) {
             try {
-                bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+                bitmap = MediaStore.Images.Media.getBitmap(
+                        getApplicationContext().getContentResolver(),
+                        data.getData()
+                );
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
@@ -119,24 +165,19 @@ public class CreateRecipeActivity extends AppCompatActivity implements View.OnCl
         if (bitmap != null) mRecipeImage.setImageBitmap(bitmap);
     }
 
-    private void onCaptureImageResult(Intent data) {
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        if (bitmap != null) {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
-            File fileDestination = new File(Environment.getExternalStorageDirectory(), System.currentTimeMillis() + ".jpg");
-            FileOutputStream fileOutputStream;
-
-            try {
-                fileDestination.createNewFile();
-                fileOutputStream = new FileOutputStream(fileDestination);
-                fileOutputStream.write(byteArrayOutputStream.toByteArray());
-                fileOutputStream.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            mRecipeImage.setImageBitmap(bitmap);
-        }
+    private void onCaptureImageResult() {
+        File file = new File(Environment.getExternalStorageDirectory() + File.separator + mCurrentTimeMillis + "image.jpg");
+        Bitmap bitmap = decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 700);
+        galleryAddPic(file.getAbsolutePath());
+        mRecipeImage.setImageBitmap(bitmap);
+        mRecipeImage.setScaleType(ImageView.ScaleType.FIT_XY);
     }
 
+    private void galleryAddPic(String path) {
+        Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        File file = new File(path);
+        Uri contentUri = Uri.fromFile(file);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
 }
