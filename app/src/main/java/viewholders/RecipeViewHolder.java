d3ec0f794cb7e2b862228dc225cacd3311da48ef
Tobias <qenzoz@gmail.com>
2016-06-21 03:05:49 +0200

    Finished RecipeViewHolder for now

--- app/src/main/java/viewholders/RecipeViewHolder.java
+++ app/src/main/java/viewholders/RecipeViewHolder.java
@@ -16,29 +16,52 @@ import util.Utilities;
  * Created by Tobias on 20-06-2016.
  */
 public class RecipeViewHolder extends RecyclerView.ViewHolder {
-    public TextView title;
-    public ImageView thumbnail;
-    public TextView progress;
-    public TextView timer;
+    private TextView mRecipeTitle;
+    private ImageView mRecipeThumbnail;
+    private TextView mRecipeNoThumbnail;
+    private TextView mRecipeProgress;
+    private TextView mRecipeTime;
 
     public RecipeViewHolder(View itemView) {
         super(itemView);
 
-        title = (TextView) itemView.findViewById(R.id.recipe_title);
-        thumbnail = (ImageView) itemView.findViewById(R.id.recipe_thumbnail);
-        progress = (TextView) itemView.findViewById(R.id.recipe_progress);
-        timer = (TextView) itemView.findViewById(R.id.recipe_time);
+        mRecipeTitle = (TextView) itemView.findViewById(R.id.recipe_title);
+        mRecipeThumbnail = (ImageView) itemView.findViewById(R.id.recipe_thumbnail);
+        mRecipeNoThumbnail = (TextView) itemView.findViewById(R.id.recipe_nothumbnail);
+        mRecipeProgress = (TextView) itemView.findViewById(R.id.recipe_progress);
+        mRecipeTime = (TextView) itemView.findViewById(R.id.recipe_time);
     }
 
     public void bindToPost(Context context, Recipe recipe) {
-        title.setText(recipe.title);
-//        thumbnail.setImageBitmap(Utilities.convertBase64ToBitmap(recipe.image));
-        Picasso.with(context)
-                .load("https://firebasestorage.googleapis.com/v0/b/project-2443912486229475189.appspot.com/o/c.jpg?alt=media&token=e3b250f2-ff9d-4279-ba0e-6492d5cade0c")
-                .into(thumbnail);
+        String notSpecified = context.getString(R.string.not_specified);
 
-        if (recipe.progress) progress.setText("Completed");
-        else progress.setText("In Progress");
-        timer.setText(recipe.time);
+        String image = recipe.image;
+        String title = recipe.title;
+        String time = recipe.time;
+
+        boolean progress = recipe.progress;
+        String progressText;
+
+        if (Utilities.isNullOrEmpty(image)) {
+            mRecipeThumbnail.setVisibility(View.GONE);
+            mRecipeNoThumbnail.setVisibility(View.VISIBLE);
+        } else {
+            mRecipeThumbnail.setVisibility(View.VISIBLE);
+            mRecipeNoThumbnail.setVisibility(View.GONE);
+            //        thumbnail.setImageBitmap(Utilities.convertBase64ToBitmap(recipe.image));
+            Picasso.with(context)
+                    .load("https://firebasestorage.googleapis.com/v0/b/project-2443912486229475189.appspot.com/o/c.jpg?alt=media&token=e3b250f2-ff9d-4279-ba0e-6492d5cade0c")
+                    .into(mRecipeThumbnail);
+        }
+
+        if (Utilities.isNullOrEmpty(title)) title = notSpecified;
+        if (Utilities.isNullOrEmpty(time)) time = notSpecified;
+
+        if (progress) progressText = context.getString(R.string.view_recipe_progress_true);
+        else progressText = context.getString(R.string.view_recipe_progress_false);
+
+        mRecipeTitle.setText(title);
+        mRecipeTime.setText(time);
+        mRecipeProgress.setText(progressText);
     }
 }
